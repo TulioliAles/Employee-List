103e4fba10a1c24050d942e38ccf8d0064e50078
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-12-01 21:11:41 -0300

    Treinamento Angular

---
+++ src/app/employee-new/employee-new.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import employees from '../employees'
+
+@Component({
+  selector: 'employee-new',
+  templateUrl: './employee-new.component.html',
+  styleUrls: ['./employee-new.component.css']
+})
+export class EmployeeNewComponent implements OnInit {
+
+  name = '';
+  salary = 0;
+
+  employees = employees as any
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addEmployee(){
+    this.employees.push({name: this.name, salary: this.salary});
+    console.log(this.employees);
+  }
+}
